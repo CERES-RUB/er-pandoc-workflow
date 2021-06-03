8fb0166ecf071db3ed755ef933687f3453a772ac
Frederik Elwert <frederik.elwert@web.de>
2021-06-03 13:35:57 +0200

    Update test script to render PDF differences

--- test.sh
+++ test.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-TESTDIR="$HOME/ownCloud/ER_Workflow/test/Testdatei"
-TAG="latest"
+TESTDIR="$(pwd)/$(dirname "$0")/test"
+TAG="edge"
 
 FAILED=0
 
@@ -15,7 +15,7 @@ docker run --rm \
 
 echo -n 'Test markdown ... '
 
-diff_md=$(diff -u "$TESTDIR/import/test_article.md" "$TESTDIR/import/expected/test_article.md")
+diff_md=$(diff -u "$TESTDIR/import/expected/test_article.md" "$TESTDIR/import/test_article.md")
 
 if [ -z "$diff_md" -a $? -eq 0 ]; then
     echo '✅ success'
@@ -28,7 +28,7 @@ fi
 
 echo -n 'Test BibTeX ... '
 
-diff_bib=$(diff -u "$TESTDIR/import/test_article.bib" "$TESTDIR/import/expected/test_article.bib")
+diff_bib=$(diff -u "$TESTDIR/import/expected/test_article.bib" "$TESTDIR/import/test_article.bib")
 
 if [ -z "$diff_bib" -a $? -eq 0 ]; then
     echo '✅ success'
@@ -50,20 +50,38 @@ docker run --rm \
 
 echo -n 'Test LaTeX ... '
 
-diff_tex=$(diff -u "$TESTDIR/export/test_article.tex" "$TESTDIR/export/expected/test_article.tex")
+diff_tex=$(diff -u "$TESTDIR/export/expected/test_article.tex" "$TESTDIR/export/test_article.tex")
 
 if [ -z "$diff_tex" -a $? -eq 0 ]; then
     echo '✅ success'
-    rm "$TESTDIR/export/test_article.tex" "$TESTDIR/export/test_article.pdf"
+    rm "$TESTDIR/export/test_article.tex"
 else
     echo '❌ failed.'
     ((FAILED++))
-    echo "$diff_tex"
+    #echo "$diff_tex"
+fi
+
+echo -n 'Test PDF ... '
+
+PDF_DIFFS=0
+rm "$TESTDIR/export/test_article_img"*_diff.png
+pdftoppm -png "$TESTDIR/export/test_article.pdf" "$TESTDIR/export/test_article_img"
+for img in "$TESTDIR/export/test_article_img"*.png; do
+    compare "$TESTDIR/export/expected/$(basename "$img")" "$img" "$img"_diff.png \
+    && rm "$img"_diff.png \
+    || ((PDF_DIFFS++))
+done
+if [ $PDF_DIFFS -eq 0 ]; then
+    echo '✅ success'
+    rm "$TESTDIR/export/test_article.tex"
+else
+    echo "❌ failed, $PDF_DIFFS pages are different."
+    ((FAILED++))
 fi
 
 echo -n 'Test HTML ... '
 
-diff_html=$(diff -u "$TESTDIR/export/test_article.html" "$TESTDIR/export/expected/test_article.html")
+diff_html=$(diff -u "$TESTDIR/export/expected/test_article.html" "$TESTDIR/export/test_article.html")
 
 if [ -z "$diff_html" -a $? -eq 0 ]; then
     echo '✅ success'
@@ -71,7 +89,7 @@ if [ -z "$diff_html" -a $? -eq 0 ]; then
 else
     echo '❌ failed.'
     ((FAILED++))
-    echo "$diff_html"
+    #echo "$diff_html"
 fi
 
 
