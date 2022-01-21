47dcaf28582e34439fdb2dd6ca394a34dc24ccba
iegilmez <iegilmez@cedis.fu-berlin.de>
2022-01-21 01:28:02 +0100

    add md2html build

---
+++ compile_md2html.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+if [ -z "$1" ]; then
+    echo "Specify an input file."
+    exit -1
+fi
+
+DIR=$(dirname $0)
+cd $DIR
+
+function build_tex {
+
+    echo "Creating file $2 ... "
+
+    BASEPATH="$(dirname $1)"
+
+    pandoc "$1" \
+        --output "$2" \
+        --defaults "defaults/common.yaml" \
+        --defaults "defaults/latex.yaml" \
+        --resource-path ".:$BASEPATH" \
+    && echo "done." \
+    || echo "error!"
+
+}
+
+function build_html {
+
+    echo "Creating file $2 ... "
+
+    BASEPATH="$(dirname $1)"
+
+    pandoc "$1" \
+        --defaults "defaults/common.yaml" \
+        --defaults "defaults/html.yaml" \
+        --resource-path ".:$BASEPATH" \
+    | xsltproc --nonet --novalid \
+        filters/linktitles.xsl - \
+    | xsltproc --nonet --novalid \
+        --output "$2" \
+        filters/pandoctweaks.xsl - \
+    && echo "done." \
+    || echo "error!"
+
+}
+
+INFILE="$1"
+TEXFILE="${INFILE%.*}.tex"
+PDFFILE="${INFILE%.*}.pdf"
+HTMLFILE="${INFILE%.*}.html"
+
+build_tex "$INFILE" "$TEXFILE"
+build_html "$INFILE" "$HTMLFILE"
